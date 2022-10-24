f3e2f7f606b1b16f5a65c434141a83f941d741c1
sahil-khan-s <muhammad.ismaeel5162@gmail.com>
2022-10-24 08:49:46 -0700

    adding features

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function insert(num){
-
     document.form1.textview.value=document.form1.textview.value+num;
 }
 
@@ -14,4 +13,26 @@ function equal(){
 function backspace(){
     var exp = document.form1.textview.value;
     document.form1.textview.value = exp.substring(0,exp.length - 1);
+}
+
+
+function square() {
+	form1.textview.value = eval(form1.textview.value) * eval(form1.textview.value)
+}
+
+function sqrt() {
+	form1.textview.value = Math.sqrt(form1.textview.value);
+}
+
+function tan() {
+	form1.textview.value = Math.tan(form1.textview.value);
+}
+function sin() {
+	form1.textview.value = Math.sin(form1.textview.value);
+}
+function cos() {
+	form1.textview.value = Math.cos(form1.textview.value);
+}
+function ln() {
+	form1.textview.value = Math.log(form1.textview.value);
 }
